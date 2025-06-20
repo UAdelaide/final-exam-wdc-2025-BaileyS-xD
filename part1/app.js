30e210d99157dee6925426a14c2fefdaffe4a643
Bailey <baileystaykov@gmail.com>
2025-06-20 04:41:16 +0000

    [automatic commit] 2025-06-20 04:41:15 UTC 36.255.115.5

--- part1/app.js
+++ part1/app.js
@@ -64,11 +64,11 @@ let db;
   }
 })();
 
-// Route to return books as JSON
+// Route to return dogs as JSON
 app.get('/api/dogs', async (req, res) => {
   try {
-    const [books] = await db.execute('SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u');
-    res.json(books);
+    const [dogs] = await db.execute('SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u.user_id;');
+    res.json(dogs);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch books' });
   }
