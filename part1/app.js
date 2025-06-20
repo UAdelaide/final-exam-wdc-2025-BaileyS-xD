f25fbba9361de7808c03f388c640af0d0e597263
Bailey <baileystaykov@gmail.com>
2025-06-20 04:35:29 +0000

    [automatic commit] 2025-06-20 04:35:29 UTC 36.255.115.5

--- part1/app.js
+++ part1/app.js
@@ -37,12 +37,12 @@ let db;
     });
 
     // Insert data if table is empty
-    await db.execute(`
-    INSERT INTO books (title, author) VALUES
-    ('1984', 'George Orwell'),
-    ('To Kill a Mockingbird', 'Harper Lee'),
-    ('Brave New World', 'Aldous Huxley')
-    `);
+    await db.execute(`INSERT INTO Users (username, email, password_hash, role) VALUES
+('alice123', 'alice@example.com', 'hashed123', 'owner'),
+('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+('carol123', 'carol@example.com', 'hashed789', 'owner'),
+('johnsmith', 'john@example.com', 'passhash', 'walker'),
+('janesmith', 'jane@example.com', 'passhashedagain', 'walker');`);
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
   }
