94e8bd707332caaaad4f67f28cd3adc7e1918a4d
Bailey <baileystaykov@gmail.com>
2025-06-20 08:33:29 +0000

    [automatic commit] 2025-06-20 08:33:29 UTC 36.255.115.5

--- part2/app.js
+++ part2/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+var session = require('express-session');
 require('dotenv').config();
 
 const app = express();
@@ -7,6 +8,14 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+
+// sessuibs
+app.use(session({
+    secret: 'evilwebsite',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: 3600000 } // session expires after 1 hour
+}));
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
