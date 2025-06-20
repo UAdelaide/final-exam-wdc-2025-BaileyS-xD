1e4bc97e409432c79ab0e4e8c6cff4e0533d856f
Bailey <baileystaykov@gmail.com>
2025-06-20 09:48:30 +0000

    [automatic commit] 2025-06-20 09:48:30 UTC 36.255.115.5

--- part2/routes/index.js
+++ part2/routes/index.js
@@ -45,8 +45,7 @@ router.get('/api/dogs', async (req, res) => {
     if (rows.length === 0) {
       return res.send();
     }
-    console.log(rows);
-    res.json({ dogs: rows });
+    res.json(rows);
   } catch (error) {
     res.status(500).json({ error: 'Failed to get Dogs' });
   }
