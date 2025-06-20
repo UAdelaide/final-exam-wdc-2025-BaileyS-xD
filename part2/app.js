1ba82ffa9e5bc090c3098b9f318a4d1ea79661d3
Bailey <baileystaykov@gmail.com>
2025-06-20 07:58:08 +0000

    [automatic commit] 2025-06-20 07:58:08 UTC 36.255.115.5

--- part2/app.js
+++ part2/app.js
@@ -14,7 +14,18 @@ let db;
 
 (async () => {
   try {
-    // connect to the database
+    // Connect to MySQL without specifying a database
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '' // Set your MySQL root password
+    });
+
+    // Create the database if it doesn't exist
+    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
+    await connection.end();
+
+    // Now connect to the created database
     db = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
