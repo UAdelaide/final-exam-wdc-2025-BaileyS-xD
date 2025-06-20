c3d1a4fe62513bddfedd8a35655336a7018d7672
Bailey <baileystaykov@gmail.com>
2025-06-20 08:15:29 +0000

    [automatic commit] 2025-06-20 08:15:29 UTC 36.255.115.5

--- part2/app.js
+++ part2/app.js
@@ -1,13 +1,54 @@
 /* eslint-disable linebreak-style */
+const express = require('express');
+const path = require('path');
+var session = require('express-session');
+require('dotenv').config();
+var mysql = require('mysql');
+
+var dbConnectionPool = mysql.createPool({
+  host: 'localhost',
+  database: 'DogWalkService'
+});
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+app.use(function(req, res, next) {
+  req.pool = dbConnectionPool;
+  next();
+});
+
+// sessions
+app.use(session({
+  secret: 'evilwebsite',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false, maxAge: 3600000 } // session expires after 1 hour
+}));
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Routes
+const indexRouter = require('./routes/index');
+
+app.use('/api/walks', walkRoutes);
+app.use('/api/users', userRoutes);
+app.use('/', indexRouter);
+
+// Export the app instead of listening here
+module.exports = app;
+
 var express = require('express');
+var multer = require('multer');
 var session = require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var walkRoutes = require('./routes/walkRoutes');
-var userRoutes = require('./routes/userRoutes');
+var usersRouter = require('./routes/users');
 
 // require mysql
 var mysql = require('mysql');
@@ -17,7 +58,7 @@ const { stringify } = require('querystring');
 // setup pool to connect to mySQL server
 var dbConnectionPool = mysql.createPool({
     host: 'localhost',
-    database: 'DogWalkService'
+    database: 'forecastfashion'
 });
 
 var app = express();
@@ -43,8 +84,8 @@ app.use(session({
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
-app.use('/api/walks', walkRoutes);
-app.use('/api/users', userRoutes);
+app.use('/users', usersRouter);
 
 module.exports = app;
