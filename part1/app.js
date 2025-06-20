c360f1ca8fe9adce3489327e4c1dcaa3787c4a94
Bailey <baileystaykov@gmail.com>
2025-06-20 05:12:49 +0000

    [automatic commit] 2025-06-20 05:12:49 UTC 36.255.115.5

--- part1/app.js
+++ part1/app.js
@@ -90,7 +90,8 @@ app.get('/api/walkrequests/open', async (req, res) => {
 app.get('/api/walkers/summary', async (req, res) => {
     try {
         const [open] = await db.execute(
-            `SELECT u.username AS walker_username, COUNT(r.rating_id) AS total_ratings, AVG(r.rating) AS average_rating, COUNT(DISTINCT CASE WHEN wr.status = 'completed' AND wa.status = 'accepted' THEN wr.request_id ELSE NULL END) AS completed_walks`
+            `SELECT u.username AS walker_username, COUNT(r.rating_id) AS total_ratings, AVG(r.rating) AS average_rating, COUNT(DISTINCT CASE WHEN wr.status = 'completed' AND wa.status = 'accepted' THEN wr.request_id ELSE NULL END) AS completed_walks
+            FROM Users u LEFT JOIN WalkApplication`
         );
         res.json(open);
     } catch (err) {
