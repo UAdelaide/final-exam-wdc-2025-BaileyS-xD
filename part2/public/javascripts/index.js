1cbda8b32f3386661a2045a837fd69edbd11e880
Bailey <baileystaykov@gmail.com>
2025-06-20 08:42:19 +0000

    [automatic commit] 2025-06-20 08:42:19 UTC 36.255.115.5

--- part2/public/javascripts/index.js
+++ part2/public/javascripts/index.js
@@ -9,7 +9,8 @@ function loginSubmit(){
 
     xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200){
-            if (this.user.role === 'owner'){
+            
+            if (this.responseText.role === 'owner'){
                 window.location.href = "/owner-dashboard.html";
             } else if (this.user.role === 'walker'){
                 window.location.href = "/walker-dashboard.html";
