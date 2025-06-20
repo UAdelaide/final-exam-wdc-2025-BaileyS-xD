bb30a8f5d96c01d6cad4a60cb55d3fcca9067402
Bailey <baileystaykov@gmail.com>
2025-06-20 08:11:04 +0000

    [automatic commit] 2025-06-20 08:11:04 UTC 36.255.115.5

--- part2/app.js
+++ part2/app.js
@@ -31,8 +31,6 @@ app.use(session({
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
-const walkRoutes = require('./routes/walkRoutes');
-const userRoutes = require('./routes/userRoutes');
 const indexRouter = require('./routes/index');
 
 app.use('/api/walks', walkRoutes);
@@ -40,4 +38,54 @@ app.use('/api/users', userRoutes);
 app.use('/', indexRouter);
 
 // Export the app instead of listening here
+module.exports = app;
+
+var express = require('express');
+var multer = require('multer');
+var session = require('express-session');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+// require mysql
+var mysql = require('mysql');
+const { connect } = require('http2');
+const { stringify } = require('querystring');
+
+// setup pool to connect to mySQL server
+var dbConnectionPool = mysql.createPool({
+    host: 'localhost',
+    database: 'forecastfashion'
+});
+
+var app = express();
+
+// middleware to access database
+app.use(function(req, res, next) {
+    req.pool = dbConnectionPool;
+    next();
+});
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// sessions
+app.use(session({
+    secret: 'evilwebsite',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: 3600000 } // session expires after 1 hour
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static('uploads'));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 module.exports = app;
