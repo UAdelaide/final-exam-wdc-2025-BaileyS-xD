2e37b4fc5d9bda569222efba47818bd44595e41a
Bailey <baileystaykov@gmail.com>
2025-06-20 06:17:04 +0000

    [automatic commit] 2025-06-20 06:17:04 UTC 36.255.115.5

--- part1/public/javascripts/index.js
+++ part1/public/javascripts/index.js
@@ -1,7 +1,7 @@
 function fetchDog(){
     fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
         const source = data.message;
         document.getElementByID('dog').src = source;
     });
