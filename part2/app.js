df18f53caeafe25adb2dcc400aa0a4197b87f201
Bailey <baileystaykov@gmail.com>
2025-06-20 08:12:25 +0000

    [automatic commit] 2025-06-20 08:12:25 UTC 36.255.115.5

--- part2/app.js
+++ part2/app.js
@@ -1,44 +1,3 @@
-/* eslint-disable linebreak-style */
-const express = require('express');
-const path = require('path');
-var session = require('express-session');
-require('dotenv').config();
-var mysql = require('mysql');
-
-var dbConnectionPool = mysql.createPool({
-  host: 'localhost',
-  database: 'DogWalkService'
-});
-
-const app = express();
-
-// Middleware
-app.use(express.json());
-
-app.use(function(req, res, next) {
-  req.pool = dbConnectionPool;
-  next();
-});
-
-// sessions
-app.use(session({
-  secret: 'evilwebsite',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false, maxAge: 3600000 } // session expires after 1 hour
-}));
-
-app.use(express.static(path.join(__dirname, '/public')));
-
-// Routes
-const indexRouter = require('./routes/index');
-
-
-app.use('/', indexRouter);
-
-// Export the app instead of listening here
-module.exports = app;
-
 var express = require('express');
 var session = require('express-session');
 var path = require('path');
@@ -88,3 +47,4 @@ app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
 module.exports = app;
+/* eslint-disable linebreak-style */
