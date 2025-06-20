7a89de8a354d3ce3704c7bf7385f01ded4bee562
Bailey <baileystaykov@gmail.com>
2025-06-20 07:39:07 +0000

    [automatic commit] 2025-06-20 07:39:07 UTC 36.255.115.5

--- part2/public/javascripts/index.js
+++ part2/public/javascripts/index.js
@@ -1,8 +1,10 @@
 // when clicking the login button, this function executes
 function loginSubmit(){
+    // get values of username and password from HTML page
     const user = document.getElementById('uname').value;
     const pass = document.getElementById('pwd').value;
 
+    // create a new AJAX request
     var xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function() {
