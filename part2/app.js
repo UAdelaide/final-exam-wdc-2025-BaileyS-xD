e0527b4eb5584eb373f46d257416decfdb625331
Bailey <baileystaykov@gmail.com>
2025-06-20 08:19:33 +0000

    [automatic commit] 2025-06-20 08:19:33 UTC 36.255.115.5

--- part2/app.js
+++ part2/app.js
@@ -71,7 +71,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 // require mysql
 var mysql = require('mysql');
