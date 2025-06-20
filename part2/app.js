be65e2db095bc2bd82a6fe8e6cce02d039ad519d
Bailey <baileystaykov@gmail.com>
2025-06-20 08:32:28 +0000

    [automatic commit] 2025-06-20 08:32:28 UTC 36.255.115.5

--- part2/app.js
+++ part2/app.js
@@ -11,9 +11,11 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+const indexRouter = require('./routes/index');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+app.use('/')
 
 // Export the app instead of listening here
 module.exports = app;
