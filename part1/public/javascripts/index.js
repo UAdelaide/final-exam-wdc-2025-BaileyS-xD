0c12a5aad5bc1cf30338a6145003dfcdadaeb86e
Bailey <baileystaykov@gmail.com>
2025-06-20 06:34:08 +0000

    [automatic commit] 2025-06-20 06:34:08 UTC 36.255.115.5

--- part1/public/javascripts/index.js
+++ part1/public/javascripts/index.js
@@ -1,3 +1,13 @@
+const { createApp, ref } = Vue
+    createApp({
+        setup() {
+        const message = ref('Hello vue!')
+        return {
+            message
+        }
+        }
+    }).mount('#app')
+
 function fetchDog(){
     fetch('https://dog.ceo/api/breeds/image/random')
     .then((response) => response.json())
