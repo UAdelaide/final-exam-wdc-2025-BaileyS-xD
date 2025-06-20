275a4d8b454d53ce4ecbb17d4a39d6fb19663562
Bailey <baileystaykov@gmail.com>
2025-06-20 05:02:38 +0000

    [automatic commit] 2025-06-20 05:02:38 UTC 36.255.115.5

--- part1/app.js
+++ part1/app.js
@@ -79,7 +79,7 @@ app.get('/api/dogs', async (req, res) => {
 // route to return open walk requests
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
-        const [open] = await db.execute(`SELECT wr.request_id, d.name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username FROM WalkRequests wr INNER JOIN Dogs d ON wr.dog_id = d.dog_id INNER JOIN Users u ON d.owner_id = u.user_id WHERE wr.status = 'open';`);
+        const [open] = await db.execute(`SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username FROM WalkRequests wr INNER JOIN Dogs d ON wr.dog_id = d.dog_id INNER JOIN Users u ON d.owner_id = u.user_id WHERE wr.status = 'open';`);
         res.json(open);
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch' });
