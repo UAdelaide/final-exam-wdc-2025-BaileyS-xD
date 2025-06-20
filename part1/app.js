80fa10e5e0ec54995abd91b70af4c2d0a346f77e
Bailey <baileystaykov@gmail.com>
2025-06-20 05:47:48 +0000

    [automatic commit] 2025-06-20 05:47:48 UTC 36.255.115.5

--- part1/app.js
+++ part1/app.js
@@ -72,6 +72,7 @@ let db;
 
     // insert ratings
     //await db.execute(`INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES (6, 2, 1, 3, 'yep'), (7, 2, 3, 5, 'yep2') `);
+    await db.execute(`INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES (6, 2, 1, 3, 'yep'), (7, 2, 3, 5, 'yep2') `);
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
   }
