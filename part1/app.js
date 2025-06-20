424eb4771f8c5fc677910a4d51c4ffdc2dd2cb7e
Bailey <baileystaykov@gmail.com>
2025-06-20 05:23:20 +0000

    [automatic commit] 2025-06-20 05:23:20 UTC 36.255.115.5

--- part1/app.js
+++ part1/app.js
@@ -63,7 +63,7 @@ let db;
     */
 
     await db.execute(`INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
-    ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', '30', 'Parklands', 'open'),`);
+    ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-09 08:00:00', '30', 'Parklands', 'closed'),`);
 
 
     // insert ratings
